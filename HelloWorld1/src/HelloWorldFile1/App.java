e3ebbb876de43e42284d8a2b2d1a065a304ab7d1
Ifra Khan <Ifra Khan@DESKTOP-MHIJI92>
2022-07-21 10:58:02 +0530

    First Commit

---
+++ HelloWorld1/src/HelloWorldFile1/App.java
@@ -0,0 +1,11 @@
+package HelloWorldFile1;
+
+public class App {
+//For Demo how to commit and create repository
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
